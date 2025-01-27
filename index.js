aa74bf8c7da8c193182b754c11f1d101123506cc
Faysalseifu <faysiseifu@gmail.com>
2025-01-27 15:34:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express()
+const app = express();
 
-mongoose.connect('mongodb://localhost:27017/admin')
+mongoose.connect('mongodb://localhost:27017/admin', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const UserSchema =mongoose.Schema({
-    name: String,
-    age: Number
-})
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
 
-const UserModel = mongoose.model("users", UserSchema)
-app.get("/getUser", (req, res)=>{
-   res.json(UserModel.find({}).then(function(users){
-    res.json(users)
-   })).catch(function(err){
-    console.log(err);
-   })
+const UserModel = mongoose.model("users", UserSchema);
 
-})
-app.listen(3001, ()=>{
-    console.log("Surer is running");
-})
+app.get("/getUser", (req, res) => {
+  UserModel.find({})
+    .then(users => {
+      res.json(users); // Send JSON response with users array
+    })
+    .catch(err => {
+      console.error("Error fetching users:", err);
+      res.status(500).json({ error: "Failed to fetch users" }); // Send error response
+    });
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on http://localhost:3001");
+});
